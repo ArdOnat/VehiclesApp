1a1bdb4dc40cbe08758e74144e16edbe9b05fc65
ardaon <arda.onat@vestel.com.tr>
2020-08-17 18:42:34 +0300

    Initial Commit

---
+++ VIPERArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VIPERArchitecture
+//
+//  Created by Arda Onat on 17.08.2020.
+//  Copyright © 2020 Arda Onat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
